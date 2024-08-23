2e4d102d03a54ac2920a1f494b4e16ea6191eeb3
Aabira Rockwater <aabira@rockwater.net>
2024-08-23 13:01:47 -0230

    push gradient colour theme

--- RockwaterWebsite/Navbar.js
+++ RockwaterWebsite/Navbar.js
@@ -14,27 +14,28 @@ window.addEventListener("scroll", function () {
 
     if (scrollTop === 0) {
       navbar.style.height = "100px";
+      // navbar.style.backgroundColor = "transparent";
+      navbar.style.background = "transparent";
       lefttNavLinks.style.color = "rgb(255, 255, 255)";
-      navbar.style.backgroundColor = "transparent";
-
       rightNavLinks.forEach(function (link) {
         link.style.fontWeight = "400";
         link.style.color = "rgb(255, 255, 255)";
       });
-      if (pathname.includes("index.html")) {
-        navbar.style.backgroundColor = "transparent";
-      } else if (pathname.includes("about.html")) {
-        // navbar.style.backgroundColor = "rgb(255, 255, 255, 0.9)";
-      } else if (pathname.includes("safety.html")) {
-        navbar.style.backgroundColor = "transparent";
-        rightNavLinks.forEach(function (link) {
-          link.style.color = "rgb(255, 255, 255)";
-        });
-        lefttNavLinks.style.color = "rgb(255, 255, 255)";
-      }
+      // if (pathname.includes("index.html")) {
+      //   navbar.style.backgroundColor = "transparent";
+      // } else if (pathname.includes("about.html")) {
+      //   // navbar.style.backgroundColor = "rgb(255, 255, 255, 0.9)";
+      // } else if (pathname.includes("safety.html")) {
+      //   navbar.style.backgroundColor = "transparent";
+      //   rightNavLinks.forEach(function (link) {
+      //     link.style.color = "rgb(255, 255, 255)";
+      //   });
+      //   lefttNavLinks.style.color = "rgb(255, 255, 255)";
+      // }
     } else {
       navbar.style.height = "60px";
-      navbar.style.backgroundColor = "rgb(153, 163, 255)";
+      // navbar.style.backgroundColor = "rgb(153, 163, 255)";
+      navbar.style.background = "linear-gradient(30deg, #fb4795, #ff984f)";
       lefttNavLinks.style.color = "white";
       rightNavLinks.forEach(function (link) {
         // link.style.textShadow = "0px 0px 5px rgba(0, 0, 0, 0.3)";
