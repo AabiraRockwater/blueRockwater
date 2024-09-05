4223dcb8b27ad7f9ebadc8c9d59585f8eb2aa46c
Aabira Rockwater <aabira@rockwater.net>
2024-09-05 11:31:50 -0230

    push

--- RockwaterWebsite/Navbar.js
+++ RockwaterWebsite/Navbar.js
@@ -1,9 +1,8 @@
 var lastScrollTop = 0;
 var navbar = document.getElementById("navbar");
 var rightNavLinks = document.querySelectorAll(".right-nav a");
-var lefttNavLinks = document.querySelector(".left-nav a");
-// var height = window.innerHeight;
-// var width = window.innerWidth;
+var leftNavLinks = document.querySelectorAll(".left-nav a");
+var dropdownLinks = document.querySelectorAll(".dropdown-menu a");
 
 window.addEventListener("scroll", function () {
   var scrollTop = document.documentElement.scrollTop || window.pageYOffset;
@@ -17,30 +16,90 @@ window.addEventListener("scroll", function () {
 
       if (scrollTop === 0) {
         navbar.style.height = "100px";
-        // navbar.style.backgroundColor = "transparent";
         navbar.style.background = "transparent";
-        lefttNavLinks.style.color = "rgb(255, 255, 255)";
+        leftNavLinks.forEach(function (link) {
+          link.style.color = "rgb(255, 255, 255)";
+        });
         rightNavLinks.forEach(function (link) {
           link.style.fontWeight = "400";
           link.style.color = "rgb(255, 255, 255)";
         });
+        dropdownLinks.forEach(function (link) {
+          link.style.fontWeight = "400";
+          link.style.color = "black"; // Maintain red color
+        });
+        if (
+          pathname.includes("product_FoodNBeverage.html") ||
+          pathname.includes("PPE.html")
+        ) {
+          navbar.style.background = "white";
+          leftNavLinks.forEach(function (link) {
+            link.style.color = "black";
+          });
+          rightNavLinks.forEach(function (link) {
+            link.style.fontWeight = "400";
+            link.style.color = "black";
+          });
+          dropdownLinks.forEach(function (link) {
+            link.style.fontWeight = "400";
+            link.style.color = "black"; // Maintain red color
+          });
+        }
       } else {
         navbar.style.height = "60px";
         navbar.style.background = "rgb(153, 163, 255)";
 
-        lefttNavLinks.style.color = "white";
+        leftNavLinks.forEach(function (link) {
+          link.style.color = "white";
+        });
         rightNavLinks.forEach(function (link) {
-          // link.style.textShadow = "0px 0px 5px rgba(0, 0, 0, 0.3)";
           link.style.fontWeight = "550";
           link.style.color = "white";
         });
+        dropdownLinks.forEach(function (link) {
+          link.style.fontWeight = "400";
+          link.style.color = "black"; // Maintain red color
+        });
+        if (
+          pathname.includes("product_FoodNBeverage.html") ||
+          pathname.includes("PPE.html")
+        ) {
+          navbar.style.background = "white";
+          leftNavLinks.forEach(function (link) {
+            link.style.color = "black";
+          });
+          rightNavLinks.forEach(function (link) {
+            link.style.fontWeight = "400";
+            link.style.color = "black";
+          });
+          dropdownLinks.forEach(function (link) {
+            link.style.fontWeight = "400";
+            link.style.color = "black"; // Maintain red color
+          });
+        }
       }
     }
 
     lastScrollTop = scrollTop;
   } else {
-    navbar.style.background = "transparent";
     navbar.style.background = "rgb(153, 163, 255)";
+    if (
+      pathname.includes("product_FoodNBeverage.html") ||
+      pathname.includes("PPE.html")
+    ) {
+      navbar.style.background = "white";
+      leftNavLinks.forEach(function (link) {
+        link.style.color = "black";
+      });
+      rightNavLinks.forEach(function (link) {
+        link.style.fontWeight = "400";
+        link.style.color = "black";
+      });
+      dropdownLinks.forEach(function (link) {
+        link.style.fontWeight = "400";
+        link.style.color = "black"; // Maintain red color
+      });
+    }
   }
 });
 
